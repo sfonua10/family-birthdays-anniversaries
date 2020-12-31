d47a9286497d7fcb353905fd8c15b42551b986ea
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2020-12-30 21:05:35 -0700

    v2 initial commit

--- components/month.js
+++ components/month.js
@@ -1,10 +1,58 @@
 export default function Month({ month, celebrants }) {
   return (
-    <div className="my-4 mx-4">
-      <h1 className="font-bold">{month}</h1>
-      {celebrants?.map((celebrant) => (
-        <p>{celebrant}</p>
-      ))}
+    <>
+    <div className="flex flex-col mt-8">
+      <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+          <table className="min-w-full">
+            <thead>
+              <tr>
+              <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xl leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  {month}
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Age
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="bg-white">
+              {celebrants.map((celebrant) => (
+                <tr>
+                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0 h-10 w-10">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                          alt=""
+                        />
+                      </div>
+
+                      <div className="ml-4">
+                        <div className="text-xl leading-5 font-medium text-gray-900">
+                          {celebrant}
+                        </div>
+                        {/* <div className="text-sm leading-5 text-gray-500">
+                          john@example.com
+                        </div> */}
+                      </div>
+                    </div>
+                  </td>
+
+                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                    <div className="text-sm leading-5 text-gray-900">
+                      33
+                    </div>
+                    {/* <div className="text-sm leading-5 text-gray-500">Web dev</div> */}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
